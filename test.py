af8c9eabdbdb58742a76f743f0d30991e699a8a3
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2022-07-12 00:33:21 +0700

    Add comments to test.py

--- test.py
+++ test.py
@@ -1,20 +1,28 @@
+from numpy import isin
 import game
 import random
 account = input("ACCOUNT: ")
 quiz = input("QUIZ NAME (1, 2, 3): ")
 
 client = game.Client(account, quiz)
+# Code chi choi 1 game. 
+# De choi duoc nhieu game, can phai kiem tra xem 
+# bang hien tai co phai ket thuc khong
 with open("log.txt", "w") as log:
     while client.playing:
         data = client.get_state()
         if isinstance(data, game.Board):
-            
+            # Lay mot move bat ky
             move = game.MOVES[random.randint(0, 3)]
+            # Viet move vao log
             log.write(f"{data.board}\n{move}\n")
+            # Make move
             print(f"{data.board}\n{move}")
             client.make_move(move)
-
+        elif isinstance(data, game.Game):
+            log.write(f"Game ended. Moves: {data.move}")
+            print("Game ended")
         elif isinstance(data, game.Result):
-            log.write(F"Session ended. Points: {data.point}")
+            log.write(f"Session ended. Points: {data.point}")
             print("Session ended")
             break
