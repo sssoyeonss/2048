b0555d5700980ca4d76afccf1072e15006df40f2
Nguyễn Minh Nhật <86871862+minhnhatnoe@users.noreply.github.com>
2022-07-18 14:58:31 +0700

    Delete log.txt in test.py for clarity

--- test.py
+++ test.py
@@ -8,23 +8,18 @@ client = game.Client(account, quiz)
 # Code chi choi 1 game.
 # De choi duoc nhieu game, can phai kiem tra xem
 # bang hien tai co phai ket thuc khong
-with open("log.txt", "w", encoding="utf-8") as log:
-    while client.playing:
-        data = client.get_state()
-        if isinstance(data, game.Board):
-            # Lay mot move bat ky
-            move = game.MOVES[random.randint(0, 3)]
-            # Viet move vao log
-            log.write(f"{data.board}\n{move}\n")
-            # Make move
-            print(f"{data.board}\n{move}")
-            client.make_move(move)
-        elif isinstance(data, game.Game):
-            # Mot game da ket thuc
-            log.write(f"Game ended. Moves: {data.move}")
-            print("Game ended")
-        elif isinstance(data, game.Result):
-            # Tro choi da ket thuc
-            log.write(f"Session ended. Points: {data.point}")
-            print("Session ended")
-            break
+while client.playing:
+    data = client.get_state()
+    if isinstance(data, game.Board):
+        # Lay mot move bat ky
+        move = game.MOVES[random.randint(0, 3)]
+        # Make move
+        print(f"{data.board}\n{move}")
+        client.make_move(move)
+    elif isinstance(data, game.Game):
+        # Mot game da ket thuc
+        print("Game ended")
+    elif isinstance(data, game.Result):
+        # Tro choi da ket thuc
+        print("Session ended")
+        break
